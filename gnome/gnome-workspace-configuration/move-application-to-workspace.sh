02a91f6f1f01d523ed51a48482ad1e9247a6603e
sandipduley <sandipp0987rahul@gmail.com>
2025-12-10 22:38:51 +0530

    ADDED: gnome navigations shortcuts script

---
+++ gnome/gnome-workspace-configuration/move-application-to-workspace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 "['<Alt>5']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 "['<Alt>6']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 "['<Alt>7']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 "['<Alt>8']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 "['<Alt>9']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Alt>0']"
+
+# to verify
+# gsettings get org.gnome.desktop.wm.keybindings move-to-workspace-5
+
---
+++ gnome/gnome-workspace-configuration/switch-to-workspace.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Control>5']" 
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Control>6']" 
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Control>7']" 
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Control>8']" 
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Control>9']" 
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Control>0']" 
+
