5156b2efdec14639336ffdb4908fe48e07270656
sandipduley <sandipp0987rahul@gmail.com>
2025-12-10 22:49:17 +0530

    ADDED: required tools

--- arch-tools.sh
+++ arch-tools.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+sudo pacman -Sy vim neovim fzf zoxide tmux zellij trash-cli yazi lazygit docker lazydocker ripgrep 7zip unrar nwg-look less fastfetch ffmpec vlc vlc-plugins-all bat flameshot jq poppler fd resvg imagemagick --needed
