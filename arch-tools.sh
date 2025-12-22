9f00d278804a4bd39148d686771d9ad047b13f56
sandipduley <sandipp0987rahul@gmail.com>
2025-12-22 18:37:03 +0530

    FIX: typo

--- arch-tools.sh
+++ arch-tools.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo pacman -Sy vim neovim fzf zoxide tmux zellij trash-cli yazi lazygit docker lazydocker ripgrep 7zip unrar nwg-look less fastfetch ffmpec vlc vlc-plugins-all bat flameshot jq poppler fd resvg imagemagick --needed
+sudo pacman -Sy vim neovim fzf zoxide tmux zellij trash-cli yazi lazygit docker lazydocker ripgrep 7zip unrar nwg-look less fastfetch ffmpeg vlc vlc-plugins-all bat flameshot jq poppler fd resvg imagemagick --needed
